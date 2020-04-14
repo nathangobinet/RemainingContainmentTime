7202181786de8b03f8125a821a56f65d5e9f5a7a
Nathan Gobinet <nathan.gobinet@gmail.com>
2020-04-14 22:35:22 +0200

    Created the application

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function hoursDiffInDays(a, b) {
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / 1000 * 60 * 60);
+}
+
+const remainingTime = (date = new Date()) => {
+  const uncontainmentDate = new Date(2020, 5, 11, 9);
+  return Math.floor((uncontainmentDate - date) / (1000 * 60 * 60 ));
+}
+
+try{
+  document.getElementById('remainingTime').innerText = `${remainingTime()} hours`;
+}catch(err){}
+
+
+exports.remainingTime = remainingTime;
---
+++ src/app.test.js
@@ -0,0 +1,7 @@
+const { remainingTime } = require('./app');
+
+test('remainingTime returns the correct remaining time', () => {
+    const uncontainmentDate = new Date(2020, 5, 11, 9);
+    const remaining = remainingTime( uncontainmentDate );
+    expect( remaining ).toBe(0);
+});
