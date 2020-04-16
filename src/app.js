c8cadc7d59eb2bca361d17a8b5cb695c965cdaba
Nathan <50440731+nathangobinet@users.noreply.github.com>
2020-04-16 09:06:10 +0200

    Add a comment

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 //this const is not that good !
 const remainingTime = (date = new Date()) => {
   const uncontainmentDate = new Date(2020, 5, 11, 9);
-  return Math.floor((uncontainmentDate - date) / (1000 * 60 * 60 * 24 ));
+  return Math.floor((uncontainmentDate - date) / (1000 * 60 * 60 ));
 }
 
+// Try to modify the remaining time
 try{
   document.getElementById('remainingTime').innerText = `${remainingTime()} hours`;
 }catch(err){}
